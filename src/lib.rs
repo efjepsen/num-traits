d7c8a1aaa9f29417b0dd6e1332777b9e92be42db
Fisher Jepsen <efjepsen@mit.edu>
2024-06-19 19:04:58 +0000

    Add restricted_std feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![feature(restricted_std)]
+
 // Copyright 2013-2014 The Rust Project Developers. See the COPYRIGHT
 // file at the top-level directory of this distribution and at
 // http://rust-lang.org/COPYRIGHT.
